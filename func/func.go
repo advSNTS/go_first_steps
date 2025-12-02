c304c5057ead80d0af2dbe2dc3b8bec5e0c19683
advSNTS <187468015+advSNTS@users.noreply.github.com>
2025-12-02 16:02:30 -0500

    fiboif

--- func/func.go
+++ func/func.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	var n int = 8
-	fmt.Printf("The %d value of fibonacci is: %d \n", n, fibo(n))
+	fmt.Printf("The %d value of fibonacci using switch is: %d \n", n, fibo(n))
+	fmt.Printf("The %d value of fibonacci using if is: %d \n", n, fiboif(n))
 }
 
 func fibo(n int) int {
@@ -17,4 +18,13 @@ func fibo(n int) int {
 	default:
 		return fibo(n-1) + fibo(n-2)
 	}
+}
+
+func fiboif(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	}
+	return fiboif(n-1) + fiboif(n-2)
 }
